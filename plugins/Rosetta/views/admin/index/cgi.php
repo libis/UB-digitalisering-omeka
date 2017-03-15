b84479253f4ddd771657606a92adf28c8982a10c
Joris <joris.lambrechts@kuleuven.be>
2017-03-15 10:23:05 +0100

    update rosetta en alma plugins + extra dc extended

--- plugins/Rosetta/views/admin/index/cgi.php
+++ plugins/Rosetta/views/admin/index/cgi.php
@@ -2,13 +2,19 @@
 <?php
     $base_url = get_option('rosetta_resolver');
     $html='';
-       
+
     if($list = rosetta_get_list($base_url."/".urlencode($_GET['search'])."/list")):
-        foreach ($list as $key => $value):                
-            $html .="<div class='rosetta_image child'><img alt='".$value."' src='".$base_url."/".$key."'/><Input type = 'Radio' Name ='pid' value= '".$key."'>
-            </div>"; 
+        foreach ($list as $key => $rep):
+            //var_dump($rep['content']);echo '<br><hr><br>';
+            $content = $rep['content'];
+            foreach($content as $fl => $file):
+              echo $fl."<br>";
+              echo "<div class='rosetta_image child'><img alt='".$file['label']."' src='".$base_url."/".$fl."'/><Input type = 'Radio' Name ='pid' value= '".$fl."'>
+              </div>";
+            endforeach;
+            echo '<br><hr><br>';          
         endforeach;
-        echo $html;
+        //echo $html;
     else:?>
     <p>No results found</p>
     <?php endif;?>
