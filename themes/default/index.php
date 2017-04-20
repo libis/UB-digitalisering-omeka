930f77253b063eb1949ab1a248d12a1f1f3e8b24
Joris <joris.lambrechts@kuleuven.be>
2017-04-20 12:54:34 +0200

    update layout homepage

--- themes/default/index.php
+++ themes/default/index.php
@@ -18,84 +18,81 @@
         </div>
     </div>-->
     <div id="content" class='container-fluid' role="main" tabindex="-1">
-        <!--<div class="row">
-            <h2 class="section-title projecten-title"><span>Tentoonstellingen</span></h2>
-            <hr class="title-hr">
-        </div> -->
-        <div class="row"><?php echo('')?>
-            <div class="card-columns exhibit-columns">
-                <div class="card featured">
-                    <div class="card-block">
-                        <div class="title-kijker">
-                            <p class="featured-tag"><span>In de kijker</span></p>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="description">Pellentesque habitant morbi tristique senectus et netus et
-                               malesuada fames ac turpis egestas. Proin malesuada tellus nulla,
-                                non tempus ipsum vehicula consectetur.</p>
+        <div class="row">
+            <div class="col-xs-12 col-sm-1 side-title">
+                <h2 class="section-title projecten-title"><span>Tentoon- stellingen</span></h2>
+            </div>
+            <div class="col-xs-12 col-sm-11">
+                <div class="card-columns exhibit-columns">
+                    <div class="card featured">
+                        <div class="card-block">
+                            <div class="title-kijker">
+                                <p class="featured-tag"><span>In de kijker</span></p>
+                                <h4 class="card-title">Donec quis ornare metus</h4>
+                                <p class="description">Pellentesque habitant morbi tristique senectus et netus et
+                                   malesuada fames ac turpis egestas. Proin malesuada tellus nulla,
+                                    non tempus ipsum vehicula consectetur.</p>
+                            </div>
+                        </div>
+                        <img class="card-img" src="<?php echo img('ph/bg4.jpg');?>" alt="Card image">
+                        <div class="to-exhibit">
+                            <p><span>Start tentoonstelling</span></p>
+                        </div>
+                     </div>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg1.jpg');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
-                    <img class="card-img" src="<?php echo img('ph/bg4.jpg');?>" alt="Card image">
-                    <div class="to-exhibit">
-                        <p><span>Start tentoonstelling</span></p>
-                    </div>
-                 </div>
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg1.jpg');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                            <p class="tag"><span>Tentoonstelling</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg5.jpg');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg5.jpg');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                            <p class="tag"><span>Tentoonstelling</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg6.png');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg6.png');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                            <p class="tag"><span>Tentoonstelling</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg6.png');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg6.png');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                            <p class="tag"><span>Tentoonstelling</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg1.jpg');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                            <p class="tag"><span>Tentoonstelling</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg1.jpg');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <hr class="column-end">
     </div>
 </section>
 <section class="exhibit-section home-section project-section general-section">
@@ -106,81 +103,82 @@
     </div>-->
     <div id="content" class='container-fluid' role="main" tabindex="-1">
         <div class="row">
-            <div class="card-columns exhibit-columns">
-                <div class="card featured">
-                  <div class="card-block">
-                      <div class="title-kijker">
-                          <p class="featured-tag"><span>In de kijker</span></p>
-                          <h4 class="card-title">Donec quis ornare metus</h4>
-                          <p class="description">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin malesuada tellus nulla, non tempus ipsum vehicula consectetur.</p>
+            <div class="col-xs-12 col-sm-1 side-title">
+                <h2 class="section-title projecten-title"><span>Projecten</span></h2>
+            </div>
+            <div class="col-xs-12 col-sm-11">
+                <div class="card-columns exhibit-columns">
+                    <div class="card featured feat-project">
+                      <div class="card-block">
+                          <div class="title-kijker">
+                              <p class="featured-tag"><span>In de kijker</span></p>
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <p class="description">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin malesuada tellus nulla, non tempus ipsum vehicula consectetur.</p>
+                          </div>
                       </div>
-                  </div>
-                  <img class="card-img" src="<?php echo img('ph/bg4.jpg');?>" alt="Card image">
-                  <div class="to-exhibit">
-                      <p><span>Naar project</span></p>
-                  </div>
-                </div>
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg1.jpg');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                            <p class="tag"><span>Project</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                      <img class="card-img" src="<?php echo img('ph/bg4.jpg');?>" alt="Card image">
+                      <div class="to-exhibit">
+                          <p><span>Naar project</span></p>
+                      </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg1.jpg');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                                <hr class="top">
+                                <h4 class="card-title">Donec quis ornare metus</h4>
+                                <hr class="bottom">
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg6.png');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                              <p class="tag"><span>Project</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg6.png');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg5.jpg');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                            <p class="tag"><span>Project</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg5.jpg');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg6.png');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                              <p class="tag"><span>Project</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg6.png');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="card">
-                    <img class="card-img" src="<?php echo img('ph/bg1.jpg');?>" alt="Card image">
-                    <div class="card-img-overlay">
-                        <div class="overlay-text">
-                            <p class="tag"><span>Project</span></p>
-                            <hr>
-                            <h4 class="card-title">Donec quis ornare metus</h4>
-                            <p class="caption"><span>Start <i class="material-icons">arrow_forward</i></span></p>
+                    <div class="card">
+                        <img class="card-img" src="<?php echo img('ph/bg1.jpg');?>" alt="Card image">
+                        <div class="card-img-overlay">
+                            <div class="overlay-text">
+                              <hr class="top">
+                              <h4 class="card-title">Donec quis ornare metus</h4>
+                              <hr class="bottom">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
        </div>
+       <hr class="column-end">
     </div>
     </div>
 </section>
@@ -193,12 +191,10 @@
         </div>
     </div>
     <div class="container">
-
         <div class="card-columns">
             <div class="card">
                 <img class="card-img-top" src="<?php echo img('ph/home_4.jpg');?>" alt="Card image cap">
                 <div class="card-block">
-
                   <h4 class="card-title">Card title that wraps to a new line</h4>
                 </div>
             </div>
@@ -228,7 +224,6 @@
                 <img class="card-img-top" src="<?php echo img('ph/home_1.jpg');?>" alt="Card image cap">
                 <div class="card-block">
                   <h4 class="card-title">Card title that wraps to a new line</h4>
-
                 </div>
             </div>
 
@@ -236,14 +231,12 @@
                 <img class="card-img-top" src="<?php echo img('ph/home_5.jpg');?>" alt="Card image cap">
                 <div class="card-block">
                   <h4 class="card-title">Card title</h4>
-
                 </div>
             </div>
             <div class="card">
                 <img class="card-img-top" src="<?php echo img('ph/home_4.jpg');?>" alt="Card image cap">
                 <div class="card-block">
                   <h4 class="card-title">Card title that wraps to a new line</h4>
-
                 </div>
             </div>
 
@@ -251,7 +244,6 @@
                 <img class="card-img-top" src="<?php echo img('ph/home_2.jpeg');?>" alt="Card image cap">
                 <div class="card-block">
                   <h4 class="card-title">Card title that wraps to a new line</h4>
-
                 </div>
             </div>
 
@@ -259,7 +251,6 @@
                 <img class="card-img-top" src="<?php echo img('ph/bg6.png');?>" alt="Card image cap">
                 <div class="card-block">
                   <h4 class="card-title">Card title that wraps to a new line</h4>
-
                 </div>
             </div>
         </div>
