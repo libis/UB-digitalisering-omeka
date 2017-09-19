bbd79cbc04804e37efa7ef63aeb689f698903de6
Joris <joris.lambrechts@kuleuven.be>
2017-09-19 14:42:42 +0200

    vaste blokken op exhibits browse

--- themes/default/exhibit-builder/exhibits/browse.php
+++ themes/default/exhibit-builder/exhibits/browse.php
@@ -36,33 +36,39 @@ echo head(array('title' => $title, 'bodyclass' => 'exhibits browse'));
   <div id="content" class='container' role="main" tabindex="-1">
       <div class="row">
         <div class="col-sm-12 col-xs-12 page">
+          <div class="row">
             <?php if (count($exhibits) > 0): ?>
-            <?php echo pagination_links(); ?>
+              <?php echo pagination_links(); ?>
 
-          <?php $exhibitCount = 0;$i=0; ?>
-          <div class="card-columns">
-          <?php foreach (loop('exhibit') as $exhibit): ?>
-              <div class="card">
-              <?php $exhibitCount++;$i++; ?>
-                <?php if ($exhibitImage = record_image($exhibit, 'fullsize',array('class'=>'card-img-top'))): ?>
-                    <?php echo $exhibitImage;?>
-                    <?php //echo exhibit_builder_link_to_exhibit($exhibit, $exhibitImage, array('class' => 'image')); ?>
-                <?php endif; ?>
+              <?php $exhibitCount = 0;$i=0; ?>
 
-                <div class="card-block browse-block page">
-                  <h3 class="card-title"><a href="<?php echo html_escape(exhibit_builder_exhibit_uri($exhibit)); ?>"><?php echo metadata($exhibit, 'title'); ?></a></h3>
-                  <?php if (($exhibitCredits = metadata('exhibit', 'credits'))): ?>
-                  <div class="exhibit-credits">
-                      <h4><?php echo $exhibitCredits; ?></h4>
+              <?php foreach (loop('exhibit') as $exhibit): ?>
+                  <div class="col-md-6 col-lg-4 col-xs-12">
+                  <div class="card">
+                    <?php $exhibitCount++;$i++; ?>
+
+                    <?php if($exhibit->cover_image_file_id):
+                      $file = $exhibit->getCoverImage();
+                    ?>
+                      <div class="card-img" style="background-image: url(<?php echo $file->getWebPath("fullsize"); ?>)"></div>
+                      <!--<img class="card-img" src="<?php echo $file->getWebPath(); ?>">-->
+                    <?php endif; ?>
+
+                    <div class="card-block browse-block page">
+                      <h3 class="card-title"><a href="<?php echo html_escape(exhibit_builder_exhibit_uri($exhibit)); ?>"><?php echo metadata($exhibit, 'title'); ?></a></h3>
+                      <?php if (($exhibitCredits = metadata('exhibit', 'credits'))): ?>
+                      <div class="exhibit-credits">
+                          <h4><?php echo $exhibitCredits; ?></h4>
+                      </div>
+                      <?php endif; ?>
+                      <?php if ($exhibitDescription = metadata('exhibit', 'description', array('no_escape' => true))): ?>
+                      <div class="card-text description"><?php echo snippet($exhibitDescription,0,150); ?></div>
+                      <?php endif; ?>
+                      <div class="card-footer">
+                          <p class="lees-meer"><a href="<?php echo html_escape(exhibit_builder_exhibit_uri($exhibit)); ?>">lees meer</a></p>
+                      </div>
+                    </div>
                   </div>
-                  <?php endif; ?>
-                  <?php if ($exhibitDescription = metadata('exhibit', 'description', array('no_escape' => true))): ?>
-                  <div class="card-text description"><?php echo snippet($exhibitDescription,0,150); ?></div>
-                  <?php endif; ?>
-                  <div class="card-footer">
-                      <p class="lees-meer"><a href="<?php echo html_escape(exhibit_builder_exhibit_uri($exhibit)); ?>">lees meer</a></p>
-                  </div>
-                </div>
               </div>
           <?php endforeach; ?>
           </div>
